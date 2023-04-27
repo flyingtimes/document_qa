bf3bc68cc089547059ce663b5115bc1355e7ab63
clarkchan <13802881820@139.com>
2023-04-27 19:28:03 +0800

    added

--- chatglm_llm.py
+++ chatglm_llm.py
@@ -7,6 +7,8 @@ class GlmLLM(LLM):
     tokenizer = AutoTokenizer.from_pretrained(model_name, local_files_only=True,trust_remote_code=True)
     model = AutoModel.from_pretrained(model_name, local_files_only=True,trust_remote_code=True).half().quantize(4).cuda()
     history = []
+    max_token = 256
+    temperature = 0.7
 
     def _call(self,
         prompt: str,
@@ -16,11 +18,10 @@ class GlmLLM(LLM):
         response, _ = self.model.chat(
             self.tokenizer,
             prompt,
-            history=history[-self.history_len:] if self.history_len > 0 else [],
             max_length=self.max_token,
             temperature=self.temperature,
         )
-        return response, history
+        return response
 
     def _identifying_params(self):
         return {"name_of_model": self.model_name}
